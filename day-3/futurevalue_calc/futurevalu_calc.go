9ba1aac3bc0b2d4d67631106750df2da241b5bdd
mksmmirhs <meraj.exe@gmail.com>
2023-12-23 19:55:19 +0600

    day 3 func

---
+++ day-3/futurevalue_calc/futurevalu_calc.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const inflationRate float64 = 2.5
+
+func main() {
+
+	var investment float64
+	var investmentRate float64
+	var years float64
+
+	fmt.Print("Enter your investment:")
+	fmt.Scan(&investment)
+
+	fmt.Print("Enter your investment rate:")
+	fmt.Scan(&investmentRate)
+
+	fmt.Print("Enter numbers of year:")
+	fmt.Scan(&years)
+
+	investmentReturn, futureRealValue := investmentReturn(investment, investmentRate, years)
+
+	fmt.Println("investment return is:", investmentReturn)
+	fmt.Println("Real value of investment:", futureRealValue)
+}
+
+func investmentReturn(investment, investmentRate, years float64) (float64, float64) {
+	ir := investment * math.Pow((1+investmentRate/100), years)
+	fr := ir / math.Pow(1+inflationRate/100, years)
+	return ir, fr
+}
---
+++ day-3/futurevalue_calc/go.mod
@@ -0,0 +1,3 @@
+module future/calc
+
+go 1.21.5
